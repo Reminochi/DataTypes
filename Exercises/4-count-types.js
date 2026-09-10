5538c5a046f90e6b6837e879d56947abc0c7352c
Reminochi <reminochikun@gmail.com>
2026-09-10 23:23:40 +0300

    Implement dynamic object properties counting

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,14 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = (arr) => {
+  const counters = {};
+
+  for (const item of arr) {
+    const type = typeof item;
+    counters[type] = (counters[type] || 0) + 1;
+  }
+
+  return counters;
+};
 
 module.exports = { countTypesInArray };
